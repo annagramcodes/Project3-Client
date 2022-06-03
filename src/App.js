ba301e20b48929d7cebcfe7c11e445c5c427dd4e
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-06-03 10:38:39 +0100

    "adds react hooc form & chakra UI"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
+import SignupPage2 from "./pages/SignupPage2";
 import LoginPage from "./pages/LoginPage";
 //import IsPrivate from './components/IsPrivate';
 import IsAnon from "./components/IsAnon";
@@ -11,64 +12,67 @@ import ProfilePage from "./pages/ProfilePage";
 import RequestPage from "./pages/RequestPage";
 import TattoosPage from "./pages/TattoosPage";
 import StylesPage from "./pages/StylesPage";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+    <ChakraProvider>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
 
-        <Route
-          path="/signup"
-          element={
-            <IsAnon>
-              <SignupPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <IsAnon>
-              <LoginPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <IsPrivate>
-              <ProfilePage />
-            </IsPrivate>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <IsPrivate>
-              <RequestPage />
-            </IsPrivate>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <IsPrivate>
-              <TattoosPage />
-            </IsPrivate>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <IsPrivate>
-              <StylesPage />
-            </IsPrivate>
-          }
-        />
-      </Routes>
-    </div>
+          <Route
+            path="/signup"
+            element={
+              <IsAnon>
+                <SignupPage2 />
+              </IsAnon>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <IsAnon>
+                <LoginPage />
+              </IsAnon>
+            }
+          />
+          <Route
+            path="/user-profile"
+            element={
+              <IsPrivate>
+                <ProfilePage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/user-profile"
+            element={
+              <IsPrivate>
+                <RequestPage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/user-profile"
+            element={
+              <IsPrivate>
+                <TattoosPage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/user-profile"
+            element={
+              <IsPrivate>
+                <StylesPage />
+              </IsPrivate>
+            }
+          />
+        </Routes>
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/pages/SignupPage2.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Button,
+  HStack,
+  FormErrorMessage,
+  Container,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
+function SignupPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const onSubmit = (data) => {
+    const { username, password, email, profileType } = data;
+    const body = { username, password, email, profileType };
+    console.log(body);
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/signup`, body)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        setErrorMessage(err.response.data.errorMessage);
+      });
+  };
+
+  return (
+    <Container maxW="md">
+      <div className="SignupPage">
+        <h1>Sign Up</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={errors.username}>
+            <FormLabel htmlFor="username">Name</FormLabel>
+            <Input type="text" {...register("username", { required: true })} />
+            {errors.username && (
+              <FormErrorMessage>Required field.</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={errors.email}>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input type="text" {...register("email", { required: true })} />
+          </FormControl>
+
+          <FormControl isInvalid={errors.password}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              type="password"
+              {...register("password", { required: true })}
+            />
+          </FormControl>
+          <FormControl isInvalid={errors.profileType}>
+            <FormLabel htmlFor="client">Are you an Artist or Client?</FormLabel>
+            <RadioGroup>
+              <HStack spacing="24px">
+                <Radio
+                  value="client"
+                  {...register("profileType", { required: true })}
+                >
+                  Client
+                </Radio>
+                <Radio
+                  value="artist"
+                  {...register("profileType", { required: true })}
+                >
+                  Artist
+                </Radio>
+              </HStack>
+            </RadioGroup>
+            {errors.profileType && (
+              <FormErrorMessage>Required field.</FormErrorMessage>
+            )}
+          </FormControl>
+          <Button type="submit">Sign Up</Button>
+        </form>
+
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <p>Already have an account?</p>
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
+      </div>
+    </Container>
+  );
+}
+
+export default SignupPage;
