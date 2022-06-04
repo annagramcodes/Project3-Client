8909aa7e3a5a0c9351660d0ce45b49439561b750
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-06-04 18:31:36 +0100

    adds checkboxes to artist sign up form

--- src/pages/ArtistSignUp.jsx
+++ src/pages/ArtistSignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
+
 import {
   VStack,
   Button,
@@ -9,6 +10,10 @@ import {
   FormLabel,
   FormErrorMessage,
   Container,
+  Checkbox,
+  CheckboxGroup,
+  Stack,
+  Flex,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
@@ -86,10 +91,52 @@ function ArtistSignUp() {
               <FormErrorMessage>Required field.</FormErrorMessage>
             )}
           </FormControl>
-
           <FormControl isInvalid={errors.styles}>
             <FormLabel htmlFor="styles">Which styles do you tattoo?</FormLabel>
-            <Input type="text" {...register("styles", { required: true })} />
+            {/* <Input type="text" {...register("styles", { required: true })} /> */}
+            <CheckboxGroup {...register("styles")} colorScheme="pink">
+              <Flex flexWrap="wrap" gap={[1, 5]} direction={["column", "row"]}>
+                <Checkbox {...register("styles")} value="blackwork">
+                  Blackwork
+                </Checkbox>
+                <Checkbox {...register("styles")} value="chicano">
+                  Chicano
+                </Checkbox>
+                <Checkbox {...register("styles")} value="dotwork">
+                  Dotwork
+                </Checkbox>
+                <Checkbox {...register("styles")} value="realism">
+                  Realism
+                </Checkbox>
+                <Checkbox {...register("styles")} value="fineline">
+                  Fineline
+                </Checkbox>
+                <Checkbox {...register("styles")} value="old-school">
+                  Old School
+                </Checkbox>
+                <Checkbox {...register("styles")} value="new-school">
+                  New School
+                </Checkbox>
+                <Checkbox {...register("styles")} value="hand-poked">
+                  Hand-Poked
+                </Checkbox>
+                <Checkbox {...register("styles")} value="geometric">
+                  Geometric
+                </Checkbox>
+                <Checkbox {...register("styles")} value="surrealism">
+                  Surrealism
+                </Checkbox>
+                <Checkbox {...register("styles")} value="lettering">
+                  Lettering
+                </Checkbox>
+                <Checkbox {...register("styles")} value="watercolor">
+                  Watercolor
+                </Checkbox>
+                <Checkbox {...register("styles")} value="other">
+                  Other
+                </Checkbox>
+              </Flex>
+            </CheckboxGroup>
 
             {errors.styles && (
               <FormErrorMessage>Required field.</FormErrorMessage>
