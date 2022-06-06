9af93a277159a0fd0609e8c3331bffa49cd5efc6
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-06-06 23:14:41 +0100

    adds delete func to images

--- src/components/RequestButton.jsx
+++
@@ -1,9 +0,0 @@
-import { Button, Link } from "@chakra-ui/react";
-import React from "react";
-import { Link as ReachLink } from "react-router-dom";
-
-function RequestButton() {
-  return <Button> Book</Button>;
-}
-
-export default RequestButton;
